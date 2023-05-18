ed3ed415d5c3ad7b17462dd53deba5cb15a71de8
sanad-bhowmik <sanadbhowmik93@gmail.com>
2023-05-18 15:25:17 +0600

    display toy details into toydetails components done

--- src/components/ToyDetails/ToyDetails.jsx
+++ src/components/ToyDetails/ToyDetails.jsx
@@ -7,22 +7,25 @@ const ToyDetails = () => {
   const { id } = useParams();
 
   useEffect(() => {
-    fetch(`http://localhost:5000/toys/${id}`)
+    fetch('http://localhost:5000/toys')
       .then((response) => response.json())
-      .then((data) => setToy(data));
+      .then((data) => {
+        const foundToy = data.find((toy) => toy._id === id);
+        setToy(foundToy);
+      });
   }, [id]);
 
   if (!toy) {
-    return <div>Loading...</div>;
+    return <div><progress className="progress w-56"></progress></div>;
   }
 
   return (
     <div>
-        <Navbar/>
+      <Navbar />
       <h1>{toy.toy_name}</h1>
       <img src={toy.img} alt={toy.toy_name} />
       <p>Price: ${toy.price}</p>
-      <p>: {toy.rating}</p>
+      <p>Rating: {toy.rating}</p>
       {/* Add more details as needed */}
     </div>
   );
