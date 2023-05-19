3a15eab27185942ac0ce66aca779c58ecb8af2dc
sanad-bhowmik <sanadbhowmik93@gmail.com>
2023-05-19 22:37:00 +0600

    trabular form into my toy  done

--- src/components/AllToys/AllToys.jsx
+++ src/components/AllToys/AllToys.jsx
@@ -41,47 +41,97 @@ const AllToys = () => {
 
     return (
         <div>
-            <Navbar />
-            <div className="banner-container mb-10">
-                <div className="banner-image">
-                    <h1 className="banner-text"><span className='text-purple-600'>All</span>Toys</h1>
-                </div>
-            </div>
-            <div className="flex items-center justify-center mb-10">
-                <input
-                    type="text"
-                    placeholder="Search toys by name"
-                    value={search}
-                    onChange={handleSearch}
-                    className="px-4 py-2 border border-gray-300 rounded-l-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-                />
-                <button
-                    onClick={handleSearchClick}
-                    type="submit"
-                    className="px-4 py-2 bg-purple-500 text-white rounded-r-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600">Search
-                </button>
-            </div>
-            <div className="grid gap-6 grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 container mx-auto mb-32">
-                {displayedToys.map((toy) => (
-                    <div key={toy._id} className="card card-side bg-base-100 shadow-xl">
-                        <figure>
-                            <img className='h-64 pt-4 pl-4 pb-4 rounded-2xl' src={toy.img} alt={toy.toy_name} />
-                        </figure>
-                        <div className="card-body text-2xl">
-                            <h2 className="card-title font-bold text-blue-500 text-3xl">{toy.toy_name}</h2>
-                            <p className="text-red-700">Price: ${toy.price}</p>
-                            <p className="text-purple-700">Sub-category: {toy.sub_categories}</p>
-                            <p className="text-green-700">Available Quantity: {toy.available_quantity}</p>
-                            <div className="card-actions justify-end">
-                                <button className="btn btn-primary text-xl text-white" onClick={() => handleViewDetails(toy)}>View Details</button>
-                            </div>
-                        </div>
-                    </div>
-                ))}
-            </div>
-            <Footer />
-            {modalOpen && <ToyDetailsModal toy={selectedToy} closeModal={() => setModalOpen(false)} />}
-        </div>
+  <Navbar />
+  <div className="banner-container mb-10">
+    <div className="banner-image">
+      <h1 className="banner-text">
+        <span className="text-purple-600">All</span>Toys
+      </h1>
+    </div>
+  </div>
+  <div className="flex items-center justify-center mb-10">
+    <input
+      type="text"
+      placeholder="Search toys by name"
+      value={search}
+      onChange={handleSearch}
+      className="px-4 py-2 border border-gray-300 rounded-l-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+    />
+    <button
+      onClick={handleSearchClick}
+      type="submit"
+      className="px-4 py-2 bg-purple-500 text-white rounded-r-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+    >
+      Search
+    </button>
+  </div>
+  <div className="container mx-auto mb-32">
+    <table className="min-w-full divide-y divide-gray-200">
+      <thead>
+        <tr>
+          <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            Image
+          </th>
+          <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            Toy Name
+          </th>
+          <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            Price
+          </th>
+          <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            Sub-category
+          </th>
+          <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            Available Quantity
+          </th>
+          <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            Seller Name
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+  {displayedToys.map((toy) => (
+    <tr key={toy._id}>
+      <td className="px-6 py-4 whitespace-nowrap">
+        <img
+          className="h-32 w-32 rounded-full"
+          src={toy.img}
+          alt={toy.toy_name}
+        />
+      </td>
+      <td className="px-6 py-4 whitespace-nowrap">
+        <div className="text-sm text-gray-900">{toy.toy_name}</div>
+      </td>
+      <td className="px-6 py-4 whitespace-nowrap">
+        <div className="text-sm text-red-700">${toy.price}</div>
+      </td>
+      <td className="px-6 py-4 whitespace-nowrap">
+        <div className="text-sm text-purple-700">{toy.sub_categories}</div>
+      </td>
+      <td className="px-6 py-4 whitespace-nowrap">
+        <div className="text-sm text-green-700">{toy.available_quantity}</div>
+      </td>
+      <td className="px-6 py-4 whitespace-nowrap">
+        <div className="text-sm text-gray-900">{toy.seller_name}</div>
+      </td>
+      <td className="px-6 py-4 whitespace-nowrap">
+        <button
+          className="btn btn-primary text-xl text-white"
+          onClick={() => handleViewDetails(toy)}
+        >
+          View Details
+        </button>
+      </td>
+    </tr>
+  ))}
+</tbody>
+
+    </table>
+  </div>
+  <Footer />
+  {modalOpen && <ToyDetailsModal toy={selectedToy} closeModal={() => setModalOpen(false)} />}
+</div>
+
     );
 };
 
--- src/components/Mytoys/Mytoys.jsx
+++ src/components/Mytoys/Mytoys.jsx
@@ -2,9 +2,10 @@ import React, { useContext, useEffect, useState } from 'react';
 import Navbar from '../Navbar/Navbar';
 import useTitle from '../../hooks/useTitle';
 import { AuthContext } from '../providers/AuthProviders';
+import Footer from '../Footer/Footer';
 
 const Mytoys = () => {
-    useTitle('MyToys')
+    useTitle('MyToys');
     const { user } = useContext(AuthContext);
     const [toys, setToys] = useState([]);
 
@@ -17,16 +18,79 @@ const Mytoys = () => {
         if (user) {
             fetch(url)
                 .then(res => res.json())
-                .then(data => console.log(data))
+                .then(data => setToys(data));
         }
-    }, [user]) // Add user as a dependency to useEffect
+    }, [user]); // Add user as a dependency to useEffect
 
     return (
         <div>
             <Navbar />
+            <div className="mb-12 bg-gradient-to-l from-green-100 to-red-300 py-6">
+                <h1 className="text-5xl text-black text-center font-bold">Toys of yours</h1>
+            </div>
 
+            <div className="p-4 container mx-auto">
+                <div className="flex flex-col">
+                    <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                        <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                            <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                                <table className="w-full table-auto">
+                                    <thead>
+                                        <tr className="bg-gray-50">
+                                            <th className="px-4 py-2">Photo</th>
+                                            <th className="px-4 py-2">Toy Name</th>
+                                            <th className="px-4 py-2">Seller Name</th>
+                                            <th className="px-4 py-2">Seller Email</th>
+                                            <th className="px-4 py-2">Subcategory</th>
+                                            <th className="px-4 py-2">Price</th>
+                                            <th className="px-4 py-2">Rating</th>
+                                            <th className="px-4 py-2">Quantity</th>
+                                            <th className="px-4 py-2">Details</th>
+                                            <th className="px-4 py-2">Actions</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {toys.map(toy => (
+                                            <tr key={toy.id}>
+                                                <td className="px-4 py-2">
+                                                    <img className="w-10 h-10 rounded-full" src={toy.img} alt="Toy" />
+                                                </td>
+                                                <td className="px-4 py-2">{toy.toy_name}</td>
+                                                <td className="px-4 py-2">{toy.seller_name}</td>
+                                                <td className="px-4 py-2">{toy.seller_email}</td>
+                                                <td className="px-4 py-2">{toy.sub_categories}</td>
+                                                <td className="px-4 py-2">${toy.price}</td>
+                                                <td className="px-4 py-2">{toy.rating}</td>
+                                                <td className="px-4 py-2">{toy.available_quantity}</td>
+                                                <td className="px-4 py-2">{toy.description}</td>
+                                                <td className="px-4 py-2">
+                                                    <div className="flex space-x-2">
+                                                        <button className="bg-red-500 hover:bg-red-600 text-white font-semibold px-4 py-2 rounded">
+                                                            Delete
+                                                        </button>
+                                                        <button className="bg-blue-500 hover:bg-blue-600 text-white font-semibold px-4 py-2 rounded">
+                                                            Edit
+                                                        </button>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <Footer />
         </div>
     );
 };
 
 export default Mytoys;
+
+{/* <tbody className="bg-white divide-y divide-gray-200">
+                                        {toys.map((toy) => (
+                                            <ToyRow key={toy.id} toy={toy} />
+                                        ))}
+                                    </tbody> */}
